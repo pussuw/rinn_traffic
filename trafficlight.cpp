713b35b6f1def6fd3a62b077b39a577836655e08
Ville <ville.juven@gmail.com>
2014-05-06 00:00:21 +0300

    Added a very simple trafficlight: lets one car through at a time, regardless of direction, where its coming from.

--- trafficlight.cpp
+++ trafficlight.cpp
@@ -5,6 +5,30 @@
  *      Author: Ville
  */
 
+#include <stddef.h>
+#include <stdio.h>
+#include "trafficlight.h"
+#include <unistd.h>
 
+TrafficLight::TrafficLight()
+{
+	pthread_mutex_init(&this->red_light_mutex, NULL);
+}
 
+TrafficLight::~TrafficLight()
+{
+	pthread_mutex_destroy(&this->red_light_mutex);
+}
 
+void TrafficLight::TrafficLightWait(int car_id)
+{
+	/* Check if light is green, if not, stop here */
+	printf("Auto %d, saapui liikennevaloihin\n", car_id);
+	pthread_mutex_lock(&this->red_light_mutex);
+	/* Were through */
+	printf("Auto %d, risteysalueella\n", car_id);
+	usleep(traffic_light_len);
+	/* Were over, release mutex */
+	printf("Auto %d, poistui liikennevaloista\n", car_id);
+	pthread_mutex_unlock(&this->red_light_mutex);
+}
--- trafficlight.h
+++ trafficlight.h
@@ -8,8 +8,20 @@
 #ifndef TRAFFICLIGHT_H_
 #define TRAFFICLIGHT_H_
 
+#include <pthread.h>
 
+/* How long it takes to get over crossing */
+#define TRAFFIC_LIGHT_CROSS_TIME	1000
 
-
+class TrafficLight
+{
+	public:
+							TrafficLight();
+		virtual 			~TrafficLight();
+		void 				TrafficLightWait(int car_id);
+	protected:
+		pthread_mutex_t		red_light_mutex;
+		static const int	traffic_light_len = TRAFFIC_LIGHT_CROSS_TIME;
+};
 
 #endif /* TRAFFICLIGHT_H_ */
