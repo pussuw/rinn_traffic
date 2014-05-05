f3c2682e4cdac03fb698cc3dcc2058e1e011528a
Ville <ville.juven@gmail.com>
2014-05-05 21:29:20 +0300

    Add bare files for traffic light

---
+++ trafficlight.cpp
@@ -0,0 +1,10 @@
+/*
+ * trafficlight.cpp
+ *
+ *  Created on: 5.5.2014
+ *      Author: Ville
+ */
+
+
+
+
---
+++ trafficlight.h
@@ -0,0 +1,15 @@
+/*
+ * trafficlight.h
+ *
+ *  Created on: 5.5.2014
+ *      Author: Ville
+ */
+
+#ifndef TRAFFICLIGHT_H_
+#define TRAFFICLIGHT_H_
+
+
+
+
+
+#endif /* TRAFFICLIGHT_H_ */
