0c72a9b38822f68d4c2d6da21c63e835c4073bbe
Ville <ville.juven@gmail.com>
2014-05-05 22:16:05 +0300

    Removed dead code + added some comments: Is it necessary to use mutex protection on synchronization signal, when no shared data is actually being shared between threads. Only the signal: I'm ready! Go ahead!

--- ferry.cpp
+++ ferry.cpp
@@ -10,6 +10,7 @@
 #include <stddef.h>
 #include <unistd.h>
 #include "ferry.h"
+
 /* How many ticks to cross the lake */
 #define LAKE_CROSS_TICKS 10
 
@@ -17,14 +18,12 @@ Ferry::Ferry()
 {
 	sem_init(&this->ferry_load, 0, 0);
 	sem_init(&this->ferry_unload, 0, 0);
-	//ferry_sync_mutex = PTHREAD_MUTEX_INITIALIZER;
-	//vehicle_ready_mutex = PTHREAD_MUTEX_INITIALIZER;
 	pthread_mutex_init(&this->ferry_sync_mutex, NULL);
 	pthread_mutex_init(&this->vehicle_ready_mutex, NULL);
 	pthread_cond_init(&this->ferry_sync_event, NULL);
 	pthread_cond_init(&this->vehicle_ready_event, NULL);
 	lake_cross_ticks = LAKE_CROSS_TICKS;
-	/* Ferry sync event and register to observer */
+	/** \todo Ferry sync event and register to observer */
 }
 
 Ferry::~Ferry()
@@ -49,6 +48,9 @@ void Ferry::UseFerry(int customer_id)
 	sem_wait(&this->ferry_unload);
 	/* 4: Inform ferry that we have unloaded */
 	printf("Auto %d, poistuu lautalta\n", customer_id);
+	/** \todo is mutex lock/unlock necessary here, I think not
+	 * when there is no shared data actually being signaled to be ready
+	 * for processing, only signal for other thread to advance */
 	pthread_cond_signal(&this->vehicle_ready_event);
 	/* 5: Return from this function -> ferry travel complete !*/
 }
@@ -128,54 +130,10 @@ void Ferry::UnloadFerry()
 	vehicles_waiting = abs(vehicles_waiting);
 	while(vehicles_waiting--)
 	{
-		/* Let everyone out */
+		/* Let one car out */
 		sem_post(&this->ferry_unload);
 		/* Wait for current car to get off board */
 		pthread_cond_wait(&this->vehicle_ready_event,
 						  &this->vehicle_ready_mutex);
 	}
 }
-
-
-#if 0
-void Ferry::LoadFerry()
-{
-	/* Get every car on board */
-	int vehicles_waiting = 0;
-	/* See if anyone is waiting to board (at this point) */
-	sem_getvalue(&this->ferry_load, &vehicles_waiting);
-	/* If a vehicle arrives after the query, too bad, he's out of the ferry */
-	if(vehicles_waiting == 0)
-		return;
-	/* At this point semaphore value is < 0 */
-	vehicles_waiting = abs(vehicles_waiting);
-	while(vehicles_waiting--)
-	{
-		/* Let everyone in */
-		sem_post(&this->ferry_load);
-		/* Wait for current car to get onboard */
-		pthread_cond_wait(&this->vehicle_ready_event,
-						  &this->vehicle_ready_mutex);
-	}
-}
-#endif
-
-#if 0
-printf("Autoja odottamassa %d\n", vehicles_waiting);
-/* Leave semaphore locked ("owned" by ferry) */
-do
-{
-	/* Wait until all vehicles have boarded the vessel */
-	sem_getvalue(&this->ferry_unload, &vehicles_in);
-	usleep(100);
-} while(vehicles_waiting != abs(vehicles_in));
-printf("Autoja sisassa %d\n", abs(vehicles_in));
-
-
-/* Unload every boarded vehicle */
-while(sem_trywait(&this->ferry_unload) < 0)
-{
-	printf("Autoa ulos\n");
-	sem_post(&this->ferry_unload);
-}
-#endif
